17e909a0b2f3663cf875a227dcfafc5b2664eb85
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2023-04-14 22:43:02 +0100

    Build XIV

--- app/src/main/kotlin/com/hilal/widget/HilalWidgetProvider.kt
+++ app/src/main/kotlin/com/hilal/widget/HilalWidgetProvider.kt
@@ -4,6 +4,7 @@ import android.appwidget.AppWidgetProvider
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.widget.RemoteViews
+import kotlin.concurrent.thread
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.File
@@ -31,12 +32,14 @@ class HilalWidgetProvider : AppWidgetProvider() {
         val dateJson = File(context.filesDir, "dates.json")
 
         if (!dateJson.exists()) {
-            val client = OkHttpClient.Builder().build()
-            val request = Request.Builder()
-                .url("http://localhost:8000/hilal-months.json")
-                .build()
-            val response = client.newCall(request).execute()
-            dateJson.writeText(response!!.body!!.toString())
+            thread {
+                val client = OkHttpClient.Builder().build()
+                val request = Request.Builder()
+                    .url("http://localhost:8000/hilal-months.json")
+                    .build()
+                val response = client.newCall(request).execute()
+                dateJson.writeText(response!!.body!!.toString())
+            }
         }
 
         return dateJson.exists().toString()
